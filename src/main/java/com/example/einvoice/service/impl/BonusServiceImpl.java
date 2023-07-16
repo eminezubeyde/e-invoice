c92c84d7520c21a9eae3ceb2e9b6b13abec7ce83
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-07-16 16:39:13 +0300

    updated

--- src/main/java/com/example/einvoice/service/impl/BonusServiceImpl.java
+++ src/main/java/com/example/einvoice/service/impl/BonusServiceImpl.java
@@ -2,11 +2,19 @@ package com.example.einvoice.service.impl;
 
 import com.example.einvoice.core.requests.BonusRequest;
 import com.example.einvoice.core.result.GeneralResult;
+import com.example.einvoice.repository.BonusRepository;
 import com.example.einvoice.service.BonusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class BonusServiceImpl implements BonusService {
+    private final BonusRepository bonusRepository;
     @Override
     public GeneralResult create(BonusRequest bonusRequest) {
+
+
         return null;
     }
 
